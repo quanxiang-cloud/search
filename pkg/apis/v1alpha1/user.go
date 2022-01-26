63452da3cca7098fa064b5381fa92b43c3cb538b
vvlgo <29571935@qq.com>
2022-01-26 21:13:13 +0800

    fix: update schema and struct

--- pkg/apis/v1alpha1/user.go
+++ pkg/apis/v1alpha1/user.go
@@ -6,13 +6,19 @@ type User struct {
 	Phone     string `json:"phone,omitempty"`
 	Email     string `json:"email,omitempty"`
 	CreatedAt int64  `json:"createdAt,omitempty"`
-
+	JobNumber string `json:"job_number,omitempty" ` //工号
+	Avatar    string `json:"avatar,omitempty" `     //头像
+	UseStatus int    `json:"use_status,omitempty" ` //状态：1正常，-2禁用，-3离职，-1删除，2激活==1 （与账号库相同）
+	TenantID  string `json:"tenant_id,omitempty" `  //租户id
+	Gender    int    `json:"gender,omitempty" `     //用户密码状态：0无，1男，2女
+	Source    string `json:"source,omitempty" `     //信息来源
+	SelfEmail string `json:"self_email,omitempty" `
 	// Departments arranged from the current user's department
 	// to the top-level department.
-	Departments []Department `json:"departments,omitempty"`
+	Departments [][]Department `json:"departments,omitempty"`
 
 	// Leaders recent leader to top leader.
-	Leaders []Leader `json:"leaders,omitempty"`
+	Leaders [][]Leader `json:"leaders,omitempty"`
 
 	Roles []Role `json:"roles,omitempty"`
 }
@@ -20,6 +26,7 @@ type User struct {
 type Department struct {
 	ID   string `json:"id,omitempty"`
 	Name string `json:"name,omitempty"`
+	PID  string `json:"pid,omitempty"`
 }
 
 type Leader struct {
