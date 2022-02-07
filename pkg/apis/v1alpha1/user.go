f4ffa31c31020e5ff991f8d088aaef1a4efe85de
vvlgo <29571935@qq.com>
2022-02-07 11:47:59 +0800

    style：typo

--- pkg/apis/v1alpha1/user.go
+++ pkg/apis/v1alpha1/user.go
@@ -6,12 +6,16 @@ type User struct {
 	Phone     string `json:"phone,omitempty"`
 	Email     string `json:"email,omitempty"`
 	CreatedAt int64  `json:"createdAt,omitempty"`
-	JobNumber string `json:"job_number,omitempty" ` //工号
-	Avatar    string `json:"avatar,omitempty" `     //头像
-	UseStatus int    `json:"use_status,omitempty" ` //状态：1正常，-2禁用，-3离职，-1删除，2激活==1 （与账号库相同）
-	TenantID  string `json:"tenant_id,omitempty" `  //租户id
-	Gender    int    `json:"gender,omitempty" `     //用户密码状态：0无，1男，2女
-	Source    string `json:"source,omitempty" `     //信息来源
+	JobNumber string `json:"job_number,omitempty" `
+	Avatar    string `json:"avatar,omitempty" `
+	//UseStatus 1:normal,-2:disable，-quit，-1：del,2:active
+	UseStatus int `json:"use_status,omitempty" `
+	//TenantID tenant id
+	TenantID string `json:"tenant_id,omitempty" ` //租户id
+	//Gender 1：man,2woman
+	Gender int `json:"gender,omitempty" `
+	//Source where the info come from
+	Source    string `json:"source,omitempty" `
 	SelfEmail string `json:"self_email,omitempty" `
 	// Departments arranged from the current user's department
 	// to the top-level department.
